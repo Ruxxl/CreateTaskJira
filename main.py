e70a879c730668577ab7648bd4e0ee11c2c1e615
Ruslan <103731221+Ruxxl@users.noreply.github.com>
2025-11-27 12:51:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ JIRA_PROJECT_KEY = os.getenv('JIRA_PROJECT_KEY', 'AS')
 JIRA_PARENT_KEY = os.getenv('JIRA_PARENT_KEY', 'AS-3150')
 JIRA_URL = os.getenv('JIRA_URL', 'https://mechtamarket.atlassian.net')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '998292747'))
-TESTERS_CHANNEL_ID = int(os.getenv('TESTERS_CHANNEL_ID', '998292747'))
+TESTERS_CHANNEL_ID = int(os.getenv('TESTERS_CHANNEL_ID', '-1002196628724'))
 
 TRIGGER_TAGS = ['#bug', '#jira']
 CHECK_TAG = '#check'
@@ -282,9 +282,6 @@ async def main():
 
     # 3) Запуск мониторинга релизов Jira (каждые 30 мин)
     asyncio.create_task(run_background_task(jira_release_check, bot, TESTERS_CHANNEL_ID, JIRA_EMAIL, JIRA_API_TOKEN, JIRA_PROJECT_KEY, JIRA_URL, logger, interval=1800))
-
-    asyncio.create_task(site_checker(bot, TESTERS_CHANNEL_ID, interval=10))
-    logger.info("Запущен site_checker в фоне")
 
     # 5) Теперь запускаем polling — он держит главный цикл
     logger.info("Запуск polling...")
